618729d9b54eb6c571ac2fe25d514cda8ef963dc
wilton-filho <wilton.filho@iftm.edu.br>
2021-04-07 11:06:48 -0300

    atualizacoes

---
+++ Ajax/aplicacao03/js/clientes.js
@@ -0,0 +1,32 @@
+function loadCustomers(sexList, customersTab) {
+    var file = "json/clientes.json";
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.status == 200 && xhttp.readyState == 4) 
+            loadInfoCustomers(xhttp, sexList, customersTab);
+    }
+    xhttp.open("GET", file, true);
+    xhttp.send();
+}
+function loadInfoCustomers(xhttp, sexList, customersTab) {
+    let tab = document.getElementById(customersTab);
+    let sexType = document.getElementById(sexList);
+
+    registro = JSON.parse(xhttp.responseText);
+
+    registro.clientes.forEach(elemento => {
+        if (elemento.sexo == sexType.value || sexType.index == 0) {
+            var line = document.createElement("tr");
+            var colName = document.createElement("td");
+            var colAge = document.createElement("td");
+            customerName = document.createTextNode(elemento.nome);
+            customerAge = document.createTextNode(elemento.idade);
+            colName.appendChild(customerName);
+            colAge.appendChild(customerAge);
+            line.appendChild(colName);
+            line.appendChild(colAge);
+            tab.appendChild(line);
+        }
+    });
+}
