52ad643fa6a51e03b73f74fe69c975ab8b139ba7
Futuresenaweb <futuresenaweb@gmail.com>
2023-02-15 11:15:47 -0300

    Update aplicacao02 (AJAX)

--- Ajax/aplicacao02/js/opcao02.js
+++ Ajax/aplicacao02/js/opcao02.js
@@ -1,4 +1,4 @@
-function getClient() {
+function getCustomersOp2() {
     let file = "https://wilton-filho.github.io/PFJS-GitHub/Ajax/aplicacao02/json/clientes.json";
     fetch(file)
         .then(response => response.json())
