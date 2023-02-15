fcbedc7cb9b64e89d9f16d04f4207291c8feed9e
Futuresenaweb <futuresenaweb@gmail.com>
2023-02-15 10:16:34 -0300

    Update aplicacao01 (AJAX)

--- Ajax/aplicacao01/js/opcao03.js
+++ Ajax/aplicacao01/js/opcao03.js
@@ -1,8 +1,6 @@
 // Opção 3: A partir da biblioteca axios
 
 function getTxtOp03() {
-    const axios = require('axios');
-
     axios.get('bd.txt')
         .then(response => {
             cdocument.getElementById("txtMsg").innerHTML = response.data + " (opção 03)"
