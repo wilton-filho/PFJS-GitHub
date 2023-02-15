9ae2f33396264e1fcd8af48149cca101cb1db864
Futuresenaweb <futuresenaweb@gmail.com>
2023-02-15 09:32:22 -0300

    Update aplicacao01 (AJAX)

---
+++ Ajax/aplicacao01/js/opcao01.js
@@ -0,0 +1,13 @@
+// Opção 1: Criação de um objeto XMLHttpRequest (o XMLHttpRequest é o objeto central que permite a comunicação entre a página web e o servidor)
+
+function getTxt() {
+    let xhttp = new XMLHttpRequest();
+    let file = "../bd.txt";
+    xhttp.onreadystatechange = function () {
+        if ((xhttp.readyState == 4) && (xhttp.status == 200)) {
+            document.getElementById("txtMsg").innerHTML = xhttp.responseText;
+        }
+    }
+    xhttp.open("GET", file, true);
+    xhttp.send();
+}
