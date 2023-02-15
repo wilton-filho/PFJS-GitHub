7d4f60662e410fc46335f22a03c86288c86fd447
Futuresenaweb <futuresenaweb@gmail.com>
2023-02-15 11:29:40 -0300

    Update aplicacao03 (AJAX)

--- Ajax/aplicacao03/js/functions.js
+++ Ajax/aplicacao03/js/functions.js
@@ -1,11 +1,11 @@
-function printCustomers() {
+function printCustomers(data) {
     let tab = document.getElementById("customersTab");
     let sexType = document.getElementById("sexList");
 
     //Apagar linhas da tabela primeiro
     document.querySelectorAll("table tbody tr").forEach(function(linha){linha.remove()})
 
-    registro.clientes.forEach(elemento => {
+    data.clientes.forEach(elemento => {
         if ((elemento.sexo == sexType.value) || (sexType.selectedIndex == 0)) {
             var line = document.createElement("tr");
             var colName = document.createElement("td");
