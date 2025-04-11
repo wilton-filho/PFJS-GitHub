fbde2d59bdf63715b136dffdf855d3456a25d43b
Futuresenaweb <futuresenaweb@gmail.com>
2025-04-11 15:31:11 -0300

    Upload aula 10.04.2025

--- Codigos_aulas/2025.01/2025.04.10_/ex01/js/main.js
+++ Codigos_aulas/2025.01/2025.04.10_/ex01/js/main.js
@@ -1,3 +1,14 @@
+import ajax from './ajax.js';
+
 window.addEventListener("DOMContentLoaded", () => {
+    // Chama as funções de requisição AJAX
+
+    // AJAX - XMLHttpRequest
+    ajax.getUsersXMLHttpRequest();
     
+    // AJAX - Fetch .then
+    ajax.getUsersFetch();
+    
+    // AJAX - Fetch async/await
+    ajax.getUsersFetchAsync();
 })
